e113a7d249bbd5acdc00799fae1f99d640614c69
beatriziferreira <biaferreiraa837@gmail.com>
2026-04-16 18:56:37 -0300

    prova

---
+++ Prova1.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+
+public class Prova1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Informe o seu nome: ");
+        String nome = scan.next();
+        System.out.println("Informe a sua classe (1-Econômica | 2-Executiva | 3-Primeira Classe");
+        int classe = scan.nextInt();
+        
+        double peso;
+        String status;
+        double pesoExcedido;
+        double taxa;
+        String mensagem;
+        
+        switch (classe) {
+            case 1:
+                System.out.println("Informe o peso da bagagem(em kg): ");
+                peso = scan.nextDouble();
+                if (peso >= 23) {
+                    status = "Bagagem Excedente!";
+                    pesoExcedido = peso - 23;
+                    taxa = pesoExcedido * 15;
+                    mensagem = "Diriga-se ao caixa para o pagamento antes do embarque.";
+                } else {
+                    status = "Dentro do limite!";
+                    taxa = 0;
+                    mensagem = "Boa viagem!";
+                    pesoExcedido = 0;
+                } 
+                System.out.println("---Check-in VooBem---");
+                System.out.println("Passageiro: " + nome);
+                System.out.println("Classe: Econômica");
+                System.out.println("Franquia: 23 kg");
+                System.out.println("Peso da bagagem: " + peso + " kg");
+                System.out.println("Status: " + status);
+                System.out.println("Excesso: " + pesoExcedido + " kg");
+                System.out.println("Taxa adicional: " + taxa + " R$");
+                System.out.println(mensagem);
+                break;
+        
+            case 2:
+                System.out.println("Informe o peso da bagagem(em kg): ");
+                peso = scan.nextDouble();
+                if (peso >= 32) {
+                    status = "Bagagem Excedente!";
+                    pesoExcedido = peso - 32;
+                    taxa = pesoExcedido * 10;
+                    mensagem = "Diriga-se ao caixa para o pagamento antes do embarque.";
+                } else {
+                    status = "Dentro do limite!";
+                    taxa = 0;
+                    mensagem = "Boa viagem!";
+                    pesoExcedido = 0;
+                } 
+                System.out.println("---Check-in VooBem---");
+                System.out.println("Passageiro: " + nome);
+                System.out.println("Classe: Executiva");
+                System.out.println("Franquia: 32 kg");
+                System.out.println("Peso da bagagem: " + peso + " kg");
+                System.out.println("Status: " + status);
+                System.out.println("Excesso: " + pesoExcedido + " kg");
+                System.out.println("Taxa adicional: " + taxa + " R$");
+                System.out.println(mensagem);
+                break;
+            
+            case 3:
+                System.out.println("Informe o peso da bagagem(em kg): ");
+                peso = scan.nextDouble();
+                if (peso >= 46) {
+                    status = "Bagagem Excedente!";
+                    pesoExcedido = peso - 46;
+                    taxa = pesoExcedido * 7;
+                    mensagem = "Diriga-se ao caixa para o pagamento antes do embarque.";
+                } else {
+                    status = "Dentro do limite!";
+                    taxa = 0;
+                    mensagem = "Boa viagem!";
+                    pesoExcedido = 0;
+                } 
+                System.out.println("---Check-in VooBem---");
+                System.out.println("Passageiro: " + nome);
+                System.out.println("Classe: Primeira classe");
+                System.out.println("Franquia: 46 kg");
+                System.out.println("Peso da bagagem: " + peso + " kg");
+                System.out.println("Status: " + status);
+                System.out.println("Excesso: " + pesoExcedido + " kg");
+                System.out.println("Taxa adicional: " + taxa + " R$");
+                System.out.println(mensagem);
+            default:
+                System.out.println("Classe inválida!");
+            break;
+        } scan.close();
+    }
+}
--- unidade4/Tabuada.java
+++ unidade4/Tabuada.java
@@ -2,9 +2,9 @@ package unidade4;
 
 public class Tabuada {
     public static void main(String[] args) {
-        for (int i = 11; i <= 20; i++) {
+        for (int i = 1; i <= 20; i++) {
             System.out.println("Tabuada do " + i + ":");
-            for (int j = 11; j <= 20; j++) {
+            for (int j = 1; j <= 20; j++) {
                 System.out.println(i + " x " + j + " = " + (i * j));
             }
             System.out.println();
